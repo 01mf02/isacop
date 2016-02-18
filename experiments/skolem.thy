6343a0ad9ff2b3be29ae6e0819a968179a3225e5
Michael Färber <michael.faerber@uibk.ac.at>
2016-02-18 18:27:12 +0100

    Fix Skolem experiments.

--- experiments/skolem.thy
+++ experiments/skolem.thy
@@ -2,47 +2,47 @@ theory skolem imports "~~/src/FOL/FOL"
 begin
 
 lemma
-  assumes c:"\<forall>x::nat. P(x)"
-  assumes b:"\<forall>y::nat. \<exists>u::nat. Q(y,u)"
-shows "\<exists>u. P(u) \<and> Q(0,u)"
+  assumes c:"\<forall>x. P(x)"
+  assumes b:"\<forall>y. \<exists>u. Q(y,u)"
+shows "\<exists>u. P(u) \<and> Q(a,u)"
 proof -
-  have d: "\<exists>y. Q(0,y)" using b by blast (* P(0, ey(0)) *)
-  obtain u where f: "Q(0,u)" using d by blast
+  have d: "\<exists>y. Q(a,y)" using b by blast (* P(0, ey(0)) *)
+  obtain u where f: "Q(a,u)" using d by blast
   have e: "P(u)" using c by blast
-  have "P(u) \<and> Q(0,u)" using e f by blast
-  thus "\<exists>u. P(u) \<and> Q(0,u)" by blast
+  have "P(u) \<and> Q(a,u)" using e f by blast
+  thus "\<exists>u. P(u) \<and> Q(a,u)" by blast
 qed
 
 lemma
-  assumes c:"\<forall>x::nat. \<not>P(x)"
-  assumes a:"\<forall>y::nat. \<exists>u::nat. R(y,u)"
-  assumes b:"\<forall>z::nat. \<exists>w::nat. Q(z,w)"
-shows "\<exists>u w. \<not>P(w) \<and> Q(u, w) \<and> R(0,u)"
+  assumes c:"\<forall>x. \<not>P(x)"
+  assumes a:"\<forall>y. \<exists>u. R(y,u)"
+  assumes b:"\<forall>z. \<exists>w. Q(z,w)"
+shows "\<exists>u w. \<not>P(w) \<and> Q(u, w) \<and> R(a,u)"
 proof -
-  have d: "\<exists>u. R(0,u)" using a by blast (* R(0, ey(0)) *)
-  then obtain u where f: "R(0,u)" by blast
+  have d: "\<exists>u. R(a,u)" using a by blast (* R(0, ey(0)) *)
+  then obtain u where f: "R(a,u)" by blast
   have "\<exists>w. Q(u, w)" using b by blast
-  then have "\<exists>w. Q(u, w) \<and> R(0,u)" using f by blast
-  then obtain w where g: "Q(u, w) \<and> R(0,u)" by blast
+  then have "\<exists>w. Q(u, w) \<and> R(a,u)" using f by blast
+  then obtain w where g: "Q(u, w) \<and> R(a,u)" by blast
   have "\<not>P(w)" using c by blast
-  then have "\<not>P(w) \<and> Q(u, w) \<and> R(0,u)" using g by blast
-  thus "\<exists>u w. \<not>P(w) \<and> Q(u, w) \<and> R(0,u)" by blast
+  then have "\<not>P(w) \<and> Q(u, w) \<and> R(a,u)" using g by blast
+  thus "\<exists>u w. \<not>P(w) \<and> Q(u, w) \<and> R(a,u)" by blast
 qed
 
 lemma
-  assumes c:"\<forall>x::nat. P(x)"
-  assumes a:"\<forall>y::nat. \<exists>u::nat. R(y,u)"
-  assumes b:"\<forall>z::nat. \<exists>w::nat. Q(z,w)"
-shows "\<exists>u w. P(w) \<and> Q(u, w) \<and> R(0,u)"
+  assumes c:"\<forall>x. P(x)"
+  assumes a:"\<forall>y. \<exists>u. R(y,u)"
+  assumes b:"\<forall>z. \<exists>w. Q(z,w)"
+shows "\<exists>u w. P(w) \<and> Q(u, w) \<and> R(a,u)"
 proof -
-  have d: "\<exists>u. R(0,u)" using a by blast (* R(0, ey(0)) *)
-  then obtain u where f: "R(0,u)" by blast
+  have d: "\<exists>u. R(a,u)" using a by blast (* R(0, ey(0)) *)
+  then obtain u where f: "R(a,u)" by blast
   have "\<exists>w. Q(u, w)" using b by blast
-  then have "\<exists>w. Q(u, w) \<and> R(0,u)" using f by blast
-  then obtain w where g: "Q(u, w) \<and> R(0,u)" by blast
+  then have "\<exists>w. Q(u, w) \<and> R(a,u)" using f by blast
+  then obtain w where g: "Q(u, w) \<and> R(a,u)" by blast
   have "P(w)" using c by blast
-  then have "P(w) \<and> Q(u, w) \<and> R(0,u)" using g by blast
-  thus "\<exists>u w. P(w) \<and> Q(u, w) \<and> R(0,u)" by blast
+  then have "P(w) \<and> Q(u, w) \<and> R(a,u)" using g by blast
+  thus "\<exists>u w. P(w) \<and> Q(u, w) \<and> R(a,u)" by blast
 qed
 
 
@@ -62,4 +62,3 @@ proof (rule ccontr)
     then show False using spec[OF spec[OF assms(1)]] by contradiction
   qed
 qed
-
